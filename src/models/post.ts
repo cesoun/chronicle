3e1e043541eea3062fb4a96a57c3e874046928e8
Christopher Oswald <chrisozzynp@gmail.com>
2022-02-05 19:35:13 -0700

    Update post.ts
    Added expected interfaces and method signatures based on the ones used
    in user.ts.
    
    Start of #7

--- src/models/post.ts
+++ src/models/post.ts
@@ -1,9 +1,33 @@
 export interface IPost {
 	id?: number;
 	author_id?: number;
+	title?: string;
 	content?: string;
 	created_at?: Date;
 	modified_at?: Date;
+}
+
+export interface PostDTO {
+	author?: string;
+	author_first?: string;
+	author_last?: string;
+	title?: string;
+	content?: string;
+	created_at?: Date;
+	modified_at?: Date;
+}
+
+export interface PostUpdateDTO {
+	title?: string;
+	content?: string;
+	created_at?: Date;
+	modified_at?: Date;
+}
+
+export interface PostQueryResult {
+	error?: boolean;
+	message?: string;
+	post?: Post;
 }
 
 export class Post {
@@ -11,5 +35,17 @@ export class Post {
 
 	constructor(post?: IPost) {
 		if (post) this.data = post;
+	}
+
+	toPostDTO(): PostDTO {
+		let dto: PostDTO = {};
+
+		return dto;
+	}
+
+	toPostUpdateDTO(): PostUpdateDTO {
+		let dto: PostUpdateDTO = {};
+
+		return dto;
 	}
 }
