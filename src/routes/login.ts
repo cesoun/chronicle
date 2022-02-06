de37b0004372af7e2248f2500c88cdb1d3b7f2ad
Christopher Oswald <chrisozzynp@gmail.com>
2022-02-05 17:05:11 -0700

    Remove PUT /login

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -1,10 +1,9 @@
 import { Router, Request, Response, NextFunction } from 'express';
-const loginRouter = Router();
-import passport from 'passport';
 import { IUser } from '../models/user';
 import { generateToken } from '../middleware/jwt';
 import { repo } from '../repository/sql';
 import { RequestErrorResponse } from '../util/errors';
+const loginRouter = Router();
 
 /* POST /login */
 loginRouter.post('/', (req: Request, res: Response, next: NextFunction) => {
@@ -63,35 +62,5 @@ loginRouter.post('/', (req: Request, res: Response, next: NextFunction) => {
 			return res.sendStatus(500);
 		});
 });
-
-/* PUT /login */
-loginRouter.put(
-	'/',
-	passport.authenticate('jwt', { session: false }),
-	(req: Request, res: Response, next: NextFunction) => {
-		if (!req.body['password'] || !req.body['new']) {
-			let err: RequestErrorResponse = {
-				error: true,
-				message: 'missing required fields: username | password',
-			};
-
-			return res.status(404).json(err);
-		}
-
-		const { username, password, email } = req.body['new'];
-
-		if (!username && !password && !email) {
-			let err: RequestErrorResponse = {
-				error: true,
-				message:
-					'missing all fields, at least one is required: username | password | email',
-			};
-
-			return res.status(404).json(err);
-		}
-
-		res.sendStatus(501);
-	}
-);
 
 export default loginRouter;
